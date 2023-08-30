41d2766a27815b0db4f758ec25080aabff5934ef
venkateshchapalamadugu27 <venki9553956591@gmail.com>
2023-08-30 18:55:07 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+?>
+
+<div class="container">
+    <header class="header">
+        <h1>Welcome to Our Custom Page</h1>
+    </header>
+
+    <section class="section">
+        <h2>About Us</h2>
+        <p>This is a custom page created using HTML, CSS, and JavaScript.</p>
+        <a href="#" class="button">Learn More</a>
+    </section>
+</div>
+
+<?php
+?>
+</body>
+</html>
